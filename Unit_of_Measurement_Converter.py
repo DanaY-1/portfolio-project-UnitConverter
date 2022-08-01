30c591fc836fd549c3f679c88470d3488211e6a9
DanaY-1 <yargesd@oregonstate.edu>
2022-08-01 16:41:51 -0700

    Updated flow rate conversion

--- Unit_of_Measurement_Converter.py
+++ Unit_of_Measurement_Converter.py
@@ -99,6 +99,30 @@ def calculate_press():
 
     #print("Enter new data and select Calculate to convert.\n")
 
+def calculate_flow():
+    try:
+        val = float(flow_input.get())
+    except:
+        print("Enter a number to convert.")
+        return
+    from_units = flow_box_value1.get()
+    to_units = flow_box_value2.get()
+
+    if from_units == "lbs/s" and to_units == "lbs/min":
+        result = round((val*60), 1)
+        flow_equation.set("Equation: lbs/min = lbs/s x 60")
+        flow_results.set("Results: " + str(val) + " "  + from_units + ' = ' + str(result) + " "  + to_units)
+    elif from_units == "lbs/min" and to_units == "lbs/s":
+        result = round(val / 60, 1)
+        flow_equation.set("Equation: lbs/s = (lbs/min) / 60")
+        flow_results.set("Results: " + str(val) + " "  + from_units + ' = ' + str(result) + " "  + to_units)
+    elif from_units == to_units:
+        flow_equation.set("")
+        flow_results.set("Results: " + str(val) + " "  + from_units + ' = ' + str(val) + " "  + to_units)
+    else:
+        flow_equation.set("")
+        flow_results.set("Error")
+
 def calculate_currency():
     try:
         val = float(currency_input.get())
@@ -260,6 +284,43 @@ lbl_pressresults = Label(tab_press, textvariable=press_results, justify='center'
 lbl_pressresults.place(relx=0.2, rely=0.7, anchor='nw')
 lbl_press_eq = Label(tab_press, textvariable=press_equation, justify='center')
 lbl_press_eq.place(relx=0.2, rely=0.5, anchor='nw')
+
+# flow rate
+tab_flow = ttk.Frame(tab_control)
+tab_control.add(tab_flow, text='Flow Rate')
+lbl_flow = Label(tab_flow, text='Convert Flow Rate Units', justify='center')
+lbl_flow.pack(side='top')
+lbl_flowto = Label(tab_flow, text='to', justify='center')
+lbl_flowto.place(relx=0.3, rely=0.2, anchor='nw')
+
+flow_input = Entry(tab_flow,width=10)
+flow_input.place(relx=0.05, rely=0.2, anchor='nw')
+
+flow_box_value1 = StringVar()
+flow_combo1 = Combobox(tab_flow, textvariable=flow_box_value1, width=5)
+flow_combo1['values'] = ("lbs/s","lbs/min")
+flow_combo1.bind('<<ComboboxSelected>>')
+flow_combo1.current(0)    #set the selected item
+flow_combo1.place(relx=0.2, rely=0.2, anchor='nw')
+
+flow_box_value2 = StringVar()
+flow_combo2 = Combobox(tab_flow, textvariable=flow_box_value2, width=5)
+flow_combo2['values'] = ("lbs/s","lbs/min")
+flow_combo2.bind('<<ComboboxSelected>>')
+flow_combo2.current(1)    #set the selected item
+flow_combo2.place(relx=0.35, rely=0.2, anchor='nw')
+
+flow_calc_btn = Button(tab_flow, text="Calculate", command=calculate_flow)
+flow_calc_btn.place(relx=0.5, rely=0.2, anchor='nw')
+
+flow_results = StringVar()
+flow_results.set("")
+flow_equation = StringVar()
+flow_equation.set("")
+lbl_flowresults = Label(tab_flow, textvariable=flow_results, justify='center')
+lbl_flowresults.place(relx=0.2, rely=0.7, anchor='nw')
+lbl_flow_eq = Label(tab_flow, textvariable=flow_equation, justify='center')
+lbl_flow_eq.place(relx=0.2, rely=0.5, anchor='nw')
 
 # currency
 tab_currency = ttk.Frame(tab_control)
